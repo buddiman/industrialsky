26962ef1debe4140f859fdc191d0cd974e2c810d
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2025-03-22 00:37:23 +0100

    add machine casings

---
+++ kubejs/startup_scripts/items/machine_casings.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('item', e => {
+    e.create('tier_1_machine_casing', 'basic').texture('industrialsky:item/tier_1_machine_case').rarity('common')
+    e.create('tier_2_machine_casing', 'basic').texture('industrialsky:item/tier_2_machine_case').rarity('uncommon')
+    e.create('tier_3_machine_casing', 'basic').texture('industrialsky:item/tier_3_machine_case').rarity('rare')
+    e.create('tier_4_machine_casing', 'basic').texture('industrialsky:item/tier_4_machine_case').rarity('epic')
+    e.create('tier_5_machine_casing', 'basic').texture('industrialsky:item/tier_5_machine_case').rarity('legend')
+    e.create('tier_6_machine_casing', 'basic').texture('kubejs:item/example_item').rarity('cosmic')
+    e.create('tier_7_machine_casing', 'basic').texture('kubejs:item/example_item').rarity('cosmic')
+})
