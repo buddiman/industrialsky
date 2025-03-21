161b12346d10703931332cbd724ca6d571f80151
Christopher Höllriegl <C.Hoellriegl@hotmail.de>
2025-03-21 16:15:50 +0100

    initial commit

---
+++ kubejs/startup_scripts/items/machine_components.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('item', e => {
+    e.create('tier_1_machine_component', 'basic').texture('modpack:item/tier_1_machine_component').rarity('common')
+    e.create('tier_2_machine_component', 'basic').texture('modpack:item/tier_2_machine_component').rarity('uncommon')
+    e.create('tier_3_machine_component', 'basic').texture('modpack:item/tier_3_machine_component').rarity('rare')
+    e.create('tier_4_machine_component', 'basic').texture('modpack:item/tier_4_machine_component').rarity('epic')
+    e.create('tier_5_machine_component', 'basic').texture('modpack:item/tier_5_machine_component').rarity('legend')
+    e.create('tier_6_machine_component', 'basic').texture('kubejs:item/example_item').rarity('cosmic')
+    e.create('tier_7_machine_component', 'basic').texture('kubejs:item/example_item').rarity('cosmic')
+})
